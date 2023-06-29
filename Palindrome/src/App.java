06ad3c3b99e3261bd6206e45780830683363b606
Prithvi Raj <prithviraj182002@gmail.com>
2023-06-29 12:43:11 +0500

    Solved exercise 16 and 17

--- Palindrome/src/App.java
+++ Palindrome/src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(isPalindrome(-11122121));
     }
+
+    public static boolean isPalindrome(int number) {
+        number = Math.abs(number);
+        int reverse = 0;
+        int remainingNumber = number;
+        do {
+            reverse *= 10; 
+            reverse += remainingNumber % 10; 
+            remainingNumber /= 10;
+        } while ( remainingNumber > 0);
+        
+        if ( number == reverse) return true;
+        return false;
+    }
+
 }
